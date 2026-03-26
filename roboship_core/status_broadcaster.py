4107c84799487e7ab5f5975e9e6b4754efbac441
yusuf869 <yusufmirza420@gmail.com>
2026-03-26 20:40:52 +0000

    Initial ROS 2 package architecture and status broadcaster

---
+++ roboship_core/status_broadcaster.py
@@ -0,0 +1,16 @@
+import rclpy
+from rclpy.node import Node
+
+class StatusBroadcaster(Node):
+    def __init__(self):
+        super().__init__('system_status_node')
+        self.get_logger().info('Roboship Core Systems: ONLINE. Awaiting Pixhawk Connection...')
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = StatusBroadcaster()
+    rclpy.spin(node)
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
